ac35d56e20809fa12246ac7377503736a6422c42
tscofield <1007833+tscofield@users.noreply.github.com>
2020-07-07 18:18:11 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,30 +1,22 @@
-from aiohttp import web
+import eventlet
 import socketio
 
-sio = socketio.AsyncServer()
-app = web.Application()
-sio.attach(app)
-
-async def index(request):
-    """Serve the client-side application."""
-    with open('index.html') as f:
-        return web.Response(text=f.read(), content_type='text/html')
+sio = socketio.Server()
+app = socketio.WSGIApp(sio, static_files={
+    '/': {'content_type': 'text/html', 'filename': 'index.html'}
+})
 
 @sio.event
 def connect(sid, environ):
-    print("connect ", sid)
+    print('connect ', sid)
 
 @sio.event
-async def chat_message(sid, data):
-    print("message ", data)
-    await sio.emit('reply', room=sid)
+def my_message(sid, data):
+    print('message ', data)
 
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
 
-app.router.add_static('/static', 'static')
-app.router.add_get('/', index)
-
 if __name__ == '__main__':
-    web.run_app(app)
+    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
