2e7a6ceafbef77917cae9e7f66c96861df1c056b
tscofield <1007833+tscofield@users.noreply.github.com>
2020-07-07 18:15:59 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template
+from aiohttp import web
+import socketio
 
-app = Flask(__name__)
+sio = socketio.AsyncServer()
+app = web.Application()
+sio.attach(app)
 
+async def index(request):
+    """Serve the client-side application."""
+    with open('index.html') as f:
+        return web.Response(text=f.read(), content_type='text/html')
 
-@app.route('/')
-def hello_whale():
-    return 'Quartermaster Game'
+@sio.event
+def connect(sid, environ):
+    print("connect ", sid)
+
+@sio.event
+async def chat_message(sid, data):
+    print("message ", data)
+    await sio.emit('reply', room=sid)
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
+app.router.add_static('/static', 'static')
+app.router.add_get('/', index)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
+    web.run_app(app)
