a2dc43fc8d7454e484458eec121c1e1bfe028d51
Tom Scofield <1007833+tscofield@users.noreply.github.com>
2020-07-11 21:37:41 -0400

    Session handling with flask-session

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_socketio import SocketIO
+from flask_session import Session
 import os
+from datetime import timedelta
 
-socketio = SocketIO()
-db = SQLAlchemy()     
-
+socketio = SocketIO(manage_session=False)
+db = SQLAlchemy()
+login_manager = LoginManager()
+sess = Session()
 
 def create_app(debug=False,redishost='localhost'):
     """Create an application."""
@@ -13,16 +17,19 @@ def create_app(debug=False,redishost='localhost'):
     app.debug = debug
     app.config['SECRET_KEY'] = 'gjr39dkjn344_!67#'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SESSION_TYPE'] = 'sqlalchemy'
+    app.config['SESSION_SQLALCHEMY'] = db
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
     
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
 
     db.init_app(app)
-    #db_setup(db)
-    #app.config['qmdb.room_members'] = db_setup(db)
-
     socketio.init_app(app)
+    login_manager.init_app(app)
+    sess.init_app(app)
 
     with app.app_context():
         db.create_all()
--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,9 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms.fields import StringField, SubmitField
 from wtforms.validators import Required
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     """Accepts a nickname and a room."""
     name = StringField('Name', validators=[Required()])
     room = StringField('Room', validators=[Required()])
