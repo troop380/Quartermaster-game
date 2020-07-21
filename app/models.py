dc1f5a76f40695f5a2ea1ba208a88a5709bb4c29
Tom Scofield <1007833+tscofield@users.noreply.github.com>
2020-07-21 19:52:21 -0400

    cleanup code to pep8 checked by flake8

--- app/models.py
+++ app/models.py
@@ -1,18 +1,21 @@
 ###############################
 # WARNING                     #
 ###############################
-# If you edit this file you will need to 
+# If you edit this file you will need to
 #  - reinitalize the database file (database.sqlite3)
 #    deleting and restarting should do it
 
 
-
 from . import db
+
 
 class room_members(db.Model):
     room = db.Column(db.String(255), primary_key=True)
     member_id = db.Column(db.String(255), primary_key=True)
     member_name = db.Column(db.String(255))
-    spectator = db.Column(db.Boolean())         # Are they playing or just hanging out
-    dm_room = db.Column(db.String(255))         # id used to send message directly to a player
 
+    # Are they playing or just hanging out
+    spectator = db.Column(db.Boolean())
+
+    # id used to send message directly to a player
+    dm_room = db.Column(db.String(255))
